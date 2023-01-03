a08d3b095e08644b3fd490fdfebe7ce4ad5ee91b
Ivan <Sipatov.Ivan@yandex.ru>
2023-01-03 15:21:55 +0400

    V1.1

---
+++ Space SHMUP Prototype/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,9 @@
+using System.Collections; //Необходим для доступа к массивам и др. коллекциям
+using System.Collections.Generic; //Необходим для доступа к спискам и словарям
+using UnityEngine; //Необходим для доустпа к Unity
+
+public class Enemy : MonoBehaviour
+{
+    [Header]
+
+}
